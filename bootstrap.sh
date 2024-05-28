4b64491071ad93fd2c4596e6eb0479d04846a49a
Bill Yang <bill.yang@live.ca>
2024-05-28 09:58:47 -0700

    - updated  command for ssh user

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 # Installs ansible
 sudo apt-get update && sudo apt-get install -y ansible
+sudo apt-get install -y sshpass
 
 # preset file contains private settings which should not goto public
 PRESET="preset"
@@ -18,6 +19,7 @@ HOSTS="$ROOTDIR/hosts"
 PLAYBOOK="$ROOTDIR/dotfiles.yml"
 
 # Runs Ansible playbook using our user.
-ansible-playbook -i "$HOSTS" "$PLAYBOOK"
+#ansible-playbook -i "$HOSTS" "$PLAYBOOK"
+ansible-playbook -i "$HOSTS" "$PLAYBOOK" --extra-vars "ansible_user=$DOTFILES_USER" --ask-pass --ask-become-pass -vvv
 
 exit 0
