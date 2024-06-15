30da01275146a2961ff18d5db61c3209a1c8150d
Bill Yang <bill.yang@live.ca>
2024-06-15 12:07:57 -0700

    - updated to support host param

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,6 +20,15 @@ PLAYBOOK="$ROOTDIR/dotfiles.yml"
 
 # Runs Ansible playbook using our user.
 #ansible-playbook -i "$HOSTS" "$PLAYBOOK"
-ansible-playbook -i "$HOSTS" "$PLAYBOOK" --extra-vars "ansible_user=$DOTFILES_USER" --ask-pass --ask-become-pass
+if [ "$#" -eq 0 ]; then          
+    ansible-playbook -i "$HOSTS" "$PLAYBOOK" --extra-vars "ansible_user=$DOTFILES_USER" --ask-pass --ask-become-pass
+else
+    host="$1"
+    ansible-playbook -i "$HOSTS" "$PLAYBOOK" --extra-vars "ansible_user=$DOTFILES_USER" --ask-pass --ask-become-pass --limit "$host"
+fi
 
 exit 0
+
+## usage:
+# ./bootstrap.sh
+# ./bootstrap.sh 192.168.0.201
